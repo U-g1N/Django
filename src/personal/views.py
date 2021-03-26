3abec371f1ff4683eb8b60bf6c0467e9926c7f84
U-g1N <eujinjoseph@gmail.com>
2021-03-26 12:23:09 +0530

    Video 11

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from personal.models import Question
 
 # Create your views here.
 
@@ -13,10 +14,13 @@ def home_screen_view(request):
     #     'some_string': "This is some string"
     # }
 
-    list_of_values = []
-    list_of_values.append("First Entry")
-    list_of_values.append("Second Entry")
-    list_of_values.append("Third Entry")
-    context['list_of_values']=list_of_values
+    # list_of_values = []
+    # list_of_values.append("First Entry")
+    # list_of_values.append("Second Entry")
+    # list_of_values.append("Third Entry")
+    # context['list_of_values']=list_of_values
+
+    questions = Question.objects.all()
+    context['questions'] = questions
 
     return render(request, "personal/home.html", context)
